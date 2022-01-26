8d28b2854ddf7207b8b5c0a0986ef47074a73198
Mentseur Fares <mentseur.fares@hotmail.fr>
2022-01-26 11:04:19 +0100

    Initialisation projet covoiturage

---
+++ src/test/java/fr/diginamic/gestiondestransportsBack/GestionDesTransportsBackApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.diginamic.gestiondestransportsBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionDesTransportsBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
